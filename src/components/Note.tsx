6afcb642b2bf9d8d571a260d80f9a8856322af9c
Andrew Aghoghovwia <androyt1@gmail.com>
2024-05-20 14:50:12 +0100

    Extracted code to jsx

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,6 +5,8 @@ import NoteForm from "./NoteForm";
 import CategoryFilter from "./CategoryFilter";
 import NoteList from "./NoteList";
 import Pagination from "./Pagination";
+import CategoryForm from "./CategoryForm";
+import EditCategoryComponent from "./EditCategoryComponent";
 
 const NoteApp = () => {
     const {
@@ -63,66 +65,24 @@ const NoteApp = () => {
         <div className='p-5 w-[450px] mx-auto bg-white rounded-xl shadow-md space-y-4'>
             <h1 className='text-2xl font-bold mb-4'>Note Taking Extension</h1>
             <NoteForm categories={categories} onSaveNote={handleSaveNote} editNote={editNote} />
-            <div className='mb-4 grid grid-cols-6 gap-2'>
-                <input
-                    type='text'
-                    value={newCategory}
-                    onChange={(e) => setNewCategory(e.target.value)}
-                    placeholder='Add new category...'
-                    className='border p-2 w-full rounded mr-2 col-span-3'
-                />
-                <button
-                    onClick={addCategory}
-                    className='bg-green-500 text-white p-2 rounded col-span-1'>
-                    Add
-                </button>
-                <button
-                    onClick={() => setShowCategoryEdit(!showCategoryEdit)}
-                    className='bg-blue-500 text-white p-2 rounded ml-2 col-span-2'>
-                    {showCategoryEdit ? "Hide Edit" : "Edit Categories"}
-                </button>
-            </div>
+
+            <CategoryForm
+                newCategory={newCategory}
+                showCategoryEdit={showCategoryEdit}
+                addCategory={addCategory}
+                setNewCategory={setNewCategory}
+                setShowCategoryEdit={setShowCategoryEdit}
+            />
             {showCategoryEdit && (
-                <div className='mb-4 '>
-                    {categories.map((category, index) => (
-                        <div key={index} className='grid grid-cols-3 mb-2'>
-                            {editCategoryIndex === index ? (
-                                <>
-                                    <input
-                                        type='text'
-                                        value={editedCategory}
-                                        onChange={(e) => setEditedCategory(e.target.value)}
-                                        className='border p-2 rounded mr-2 w-full'
-                                    />
-                                    <button
-                                        onClick={() => updateCategory(index)}
-                                        className='bg-green-500 text-white p-2 rounded mr-2'>
-                                        Update
-                                    </button>
-                                    <button
-                                        onClick={() => setEditCategoryIndex(null)}
-                                        className='bg-gray-500 text-white p-2 rounded'>
-                                        Cancel
-                                    </button>
-                                </>
-                            ) : (
-                                <>
-                                    <span className='mr-2'>{category}</span>
-                                    <button
-                                        onClick={() => setEditCategoryIndex(index)}
-                                        className='bg-blue-500 text-white p-2 rounded mr-2'>
-                                        Edit
-                                    </button>
-                                    <button
-                                        onClick={() => deleteCategory(index)}
-                                        className='bg-red-500 text-white p-2 rounded'>
-                                        Delete
-                                    </button>
-                                </>
-                            )}
-                        </div>
-                    ))}
-                </div>
+                <EditCategoryComponent
+                    categories={categories}
+                    editedCategory={editedCategory}
+                    editCategoryIndex={editCategoryIndex}
+                    setEditedCategory={setEditedCategory}
+                    updateCategory={updateCategory}
+                    setEditCategoryIndex={setEditCategoryIndex}
+                    deleteCategory={deleteCategory}
+                />
             )}
             <div className='mb-4'>
                 <input
